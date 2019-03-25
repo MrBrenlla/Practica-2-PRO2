bb56f75f633b9d9c76371511c3d68227528f2cb6
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2019-03-25 13:14:25 +0100

    Update SharedTypes.pas

--- SharedTypes.pas
+++ SharedTypes.pas
@@ -1,1 +1,23 @@
-hola
+{
+TITLE: 
+AUTHOR 1: Brais García Brenlla        
+AUTHOR 2: Javier González Rodríguez  
+DATE: 
+}
+
+unit SharedTypes;
+
+interface
+
+const
+	BLANKVOTE='B';
+	NULLVOTE='N';
+
+type
+	tPartyName=string;
+  tCenterName=string;
+	tNumVotes=word;
+	
+	implementation
+	
+end.
